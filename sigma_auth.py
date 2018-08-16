7db5ce9b69b5ecea064e4c0021f5d7901ec6cc73
Bernardo Esteves <esteveste@users.noreply.github.com>
2018-08-16 13:19:32 +0100

    probably better

--- sigma_auth.py
+++ sigma_auth.py
@@ -9,6 +9,8 @@ import time
 import subprocess
 import threading
 
+CHECK_RATE = 2  # keeps cheking every N seconds
+
 # get pass
 # input subprocess is in bytes
 passwd = getpass.unix_getpass("Sigma Password:").encode()
@@ -16,20 +18,21 @@ passwd = getpass.unix_getpass("Sigma Password:").encode()
 
 def sigma_auth():
     while(True):
-        time.sleep(3000)
-        os.system("kinit -R")
+        time.sleep(CHECK_RATE)
         if(not got_permissions()):
-            try:
-                p = subprocess.Popen(
-                    ['kinit'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-                stdout, stderr = p.communicate(input=passwd)
-                os.system('aklog')
-                time.sleep(2)  # litle pause
-                if(not got_permissions()):
-                    print(
-                        "Sigma Auth Error After Kinit -R Error Not getting permissions\n", stdout, '\n', stderr)
-            except Exception as e:
-                print("Sigma Auth Kinit Error", e)
+            os.system("kinit -R")
+            if(not got_permissions()):
+                try:
+                    p = subprocess.Popen(
+                        ['kinit'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+                    stdout, stderr = p.communicate(input=passwd)
+                    os.system('aklog')
+                    time.sleep(2)  # litle pause
+                    if(not got_permissions()):
+                        print(
+                            "Sigma Auth Error After Kinit -R Error Not getting permissions\n", stdout, '\n', stderr)
+                except Exception as e:
+                    print("Sigma Auth Kinit Error", e)
 
 
 def got_permissions():
